6a059412c9592f5717b1c547ed5ad57a6a64ea11
Fayyaz Baig <127140738+Fayyazbaig1@users.noreply.github.com>
2023-05-30 21:51:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,516 @@
+//Alerts//
+
+// alert("fayyaz");
+// alert("baig");
+// alert("fayyazbaig124@gmail.com");
+// alert("0344-0033971");
+// alert("******");
+
+// alert ("you're learning javascript!");
+
+// alert ("OH ALLAH! give me educate environment");
+
+//VARIABLES FOR STRING//
+
+// var ab = "WEB";
+// var bc = "APP";
+// var cd = "DEVELOPMENT";
+// var result = ab + " " + bc + " " + cd;
+
+// console.log(result);
+
+// var notidea = 50;
+// var idea = ("50");
+// var result = notidea + idea;
+
+// console.log(result);
+
+// var teamname = "pakistan";
+// alert("pakistan");
+
+// console.log(teamname);
+
+// var bestMan = "charlie";
+// var Man = "deriv";
+// var merge = bestMan + " " +  Man;
+// console.log(merge);
+
+// variables for numbers //
+
+// var num = 134;
+// var src = 6;
+// var newnum = 43;
+// var result = num + src * newnum;
+
+// console.log(result);
+
+// var caseqty = 144;
+// var sub = 6;
+// var add = 6;
+// var result = caseqty - sub + add ;
+// var mltp = caseqty * 9;
+// var result = parseInt(mltp);
+
+// console.log(mltp);
+
+// var dec = 20;
+// var sum = 20
+// var result = sum + 20 + dec;
+
+// console.log(result);
+
+// var merchTotal = 100;
+// var shippingCharges = 10;
+// var orderTotal = merchTotal + shippingCharges;
+
+// console.log(orderTotal);
+
+// var abc = 38;
+// abc = abc + 40;
+
+// console.log  (abc);
+
+// CHAP 4
+
+// var product_cost = 3.45;
+
+// var nameOfBand; 3.45;
+
+// var abc = 345;
+
+// var fayyaz_Baig = 142000;
+
+// LEGAL VARIABLES
+
+// name
+// age
+// _count
+// $price
+// myVar
+// userInput
+
+// ILLEGAL VARIABLES
+
+// 1name
+// var
+// my-variable
+// function
+// let
+// true
+
+// CHAP 5
+
+// var a = 50;
+// var b = 4;
+// var c = a % b;
+
+// console.log(c);
+
+// var num = 20 % 6;
+
+// console.log(num);
+
+// var largeNum = 1000 * 2000;
+
+// var num1 = 10;
+// var num2 = 5;
+// var result = num1 - num2;
+
+// console.log(result); 
+
+// var dividend = 15;
+// var divisor = 4;
+// var remainder = dividend % divisor;
+
+// console.log(remainder); 
+
+// var a = 4;
+// var b = 4;
+// alert(a * b);
+
+// CHAP 6
+
+// var x = 1
+// x = x + 1;
+
+// var x = 100;
+// x = x - 1;
+
+// var x = 50;
+// var y = x++;
+
+// console.log(y);  50
+
+// var y = 50;
+// var z = --y;
+
+// console.log(z); 49
+
+// var num = 10;
+// var newNum = num--;
+
+// var num = 10;
+// var newNum = ++num;
+
+// console.log(newNum);
+
+// var a = 9;
+// var b = ++a;
+// alert(b);
+
+// CHAP 7
+
+// var calculatedNum = 2 + (2 * 6);
+
+// console.log(calculatedNum);  14
+
+// var calculatedNum = (2 + 2) * 6;
+
+// console.log(calculatedNum);  24
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+
+// console.log(calculatedNum);  24
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+
+// console.log(calculatedNum);  18
+
+
+// var num = 2 + 2 
+// var num_b = 4 + 10;
+// var result = num * num_b;
+
+// console.log(result);
+
+// var num = 2 + 2 * 4 + 10;
+// console.log(num);
+
+// var num = 2 * 4;
+// var result =  4 / num;
+// console.log(result);
+
+// CHAP 8
+
+// var num = "2" + "2";
+// console.log(num);
+
+// var message = ("Hello," + "Dolly");
+// alert(message);
+
+// alert("33" + 3); 333
+
+// alert("pakistan" + " " + "zindabad");
+
+// var result = "The number is:";
+// alert(result + " " + 42);
+
+// var a = "fayyaz";
+// var b = "baig";
+// var result = a + " " + b;
+
+// console.log(result);
+
+// CHAP 9
+
+// var ab = prompt("enter your name");
+
+// var bb = prompt("what is your country?" , "china");
+
+// var yourName = prompt("Enter Your Name");
+
+// var userInput = prompt("Please enter your name:", "fayyaz baig");
+
+// var msg = "Please enter your name:";
+// var df= "fayyaz baig";
+
+// var userInput = prompt(msg, df);
+
+// console.log(userInput);
+
+// var a = "what is your fav colour";
+// var b = "white";
+// var c = prompt(a , b);
+
+// console.log(c);
+
+// alert("good");
+
+// CHAP 10
+
+// var city = "Karachi";
+// if (city = "Karachi") {
+// console.log("The City OF Lights");
+// }
+
+// var x = 10;
+// var y = 10;
+
+// if (x === y) {
+//   var z = prompt("Enter the value of z:");
+//   console.log(z); 
+// }
+
+// var ZipCode = "10010";
+
+// if(ZipCode == "11010"){
+//     alert("karachi");
+    
+// }else(ZipCode == "10010");
+// {
+//     alert  ( "please enter right correct city");
+// }
+
+
+// var x = 1;
+
+// if (x === 1) {
+//   x = 10;
+// }
+
+// console.log(x);
+
+// CHAP 11
+
+// var a = "Fia";
+// var b = "fia";
+// if(a !== b){
+//     alert("good luck");
+// }
+
+// var a = 9;
+// var b = 10;
+// if(b >= a){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// var a = 5;
+// if(a !== 20){
+
+//     a = 50;
+// }
+
+// console.log(a);
+
+// var num = 10;
+// if(num !== 20){
+//     console.log(true);
+//     alert("congratulations");
+// }
+
+// var que = prompt("first name")
+// var Fayyaz = "Fayyaz";
+// if(que !== Fayyaz){
+//     console.log(true);
+//     alert("no match");
+// }else{
+//     alert("match");
+// }
+
+// CHAP 12
+
+// var a = 10;
+// var b = 5
+// if(a >= b){
+//     alert(true);
+// }else{
+//     alert(false);
+// }
+
+
+// var num = parseFloat(prompt("enter your marks"));
+
+// var parcentage = (num / 1100) * 100;
+
+// var grade;
+
+// if (parcentage >= 90){
+//     grade = "A";
+// } else if(parcentage >= 80){
+//     grade = "B";
+// }else if(parcentage >= 70){
+//     grade = "C";
+// }else if(parcentage >= 60){
+//     grade = "D";
+// }else if(parcentage >= 50){
+//     grade = "E";
+// }else{
+//     grade = "F"
+// }
+
+// alert("your percentage is" + " " + parcentage.toFixed(2) + " " + "your grade is" + " " + grade);
+
+// var a = 5;
+// if (a === 10) {
+//     alert("a is 10");
+//     }else{
+//         alert("the correct value of A is 5");
+//     }
+
+// var city = prompt("enter your city?");
+// if(city === "karachi"){
+//     alert("yeah! it's karachi");
+    
+
+// }else if (city === "lahore"){
+//     alert("Huuh! it's lahore");
+
+// }else{
+//     alert("not found");
+// }
+
+// CHAP 13
+
+// var a = 10;
+// var b = 10;
+// var c = 20;
+// var d = 20;
+// if ((a === b) || (c !== d)) {
+//     alert("match");
+//   }
+  
+// var a = 70;
+// var Hamza = 70;
+
+// var b = 60;
+// var Arsalan = 50;
+
+// var age = 50;
+
+// if ((a === Hamza|| b === Arsalan) && age < 60) {
+
+//     alert("good");
+//   }
+  
+// var first = 15;
+// var second = 20;
+// if(first <= second){
+//     alert("match");
+// }
+
+// var firstName = "fayyaz";
+// var lastName = "baig";
+
+// firstName = prompt("enter your first name");
+// lastName = prompt("enter your last name");
+// if(firstName == firstName & lastName == lastName){
+//     alert("found");
+// }else{
+//     alert("not found");
+// }
+
+// CHAP 14
+
+// var password = 5;
+
+// if (password !== 3) {
+//     if (password.length <= 5) {
+//       alert("Password must be greater than 5");
+//     } else {
+//       alert("OK");
+//     }
+//   }
+  
+// var a = 1;
+// var c = "Max";
+// if (a === 1) {
+//     if (c === "Max") {
+//     alert("OK");
+//     }
+//     }
+
+// var a = 1;
+// var c = "Max";
+
+// if (a === 1 && c === "Max") {
+//     alert("OK");
+//   }
+  
+// var a = 10;
+// var b = 10;
+// if(a === b){
+//     if(a <= b){
+//         alert("passed");
+//     }
+// }
+
+// CHAP 15 I
+
+// var arr = [];
+
+// var arr = ["one"];
+
+// var alphabet = ["h","i","j","k"];
+// alert(alphabet[2]);
+
+// var alphabet=["h","i","j","k", "“l”","”m”", "“n”", "“o”"];
+// console.log(alphabet.length);
+
+// var arr = ["fayyaz"];
+// arr.push("baig");
+// alert("baig");
+
+// console.log(arr);
+
+// CHAP 16 II
+
+// var arr = ["fayyaz"];
+// arr.push("baig");
+// alert("baig");
+
+// console.log(arr);
+
+// var Alphabet=["h","i","j","k"];
+// Alphabet.pop(Alphabet[3]);
+
+// console.log(Alphabet);
+
+// var Alphabet=["h","i","j","k"];
+// Alphabet.push("l");
+
+// console.log(Alphabet);
+
+// CHAP 16 III
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+
+// console.log(sizes);
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift("1" , "2" , "3");
+
+// console.log(sizes);
+
+// var arr = ["abc"];
+// arr.unshift("bbc");
+// alert("new is bbc");
+
+// console.log(arr);
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2 , 0 , "L");
+
+// console.log(sizes);
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var arr = sizes.slice(0 , 3);
+
+// console.log(arr);
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1 , 3 , "birds" , "hen")
+
+// console.log(pets);
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1 , 2);
+
+// console.log(pets);
+
+var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+var arr = pets.slice  (3 , 5);
+
+console.log(arr);
